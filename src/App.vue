86980d5c1643ed43df2d055e38cd511e9a3ed9a1
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-07-22 14:27:11 -0400

    Latest work using vue framework, add the chart
    Update the latest work for the webcam project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+	<p>RPPG (remote photoplethysmography)</p>
+    <WebCam msg="RPPG (remote photoplethysmography)"/>
+  </div>
+</template>
+
+<script>
+import WebCam from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WebCam
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+<div>	
+  <div id = "WebCam">
+	<video autodisplay="true" width="600" height="400" id="video"></video>
+	<!--canvas id = "canvas" height = "680" width = "100%"></canvas-->
+  </div>
+  
+  <div id = "visualization"></div>
+</div>
+</template>
+
+<script>
+import run from './sketch.js'	
+
+export default {
+  name: 'WebCam',
+  props: {
+    msg: String
+  },
+  mounted() {
+	run();
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	
+
+		
+	#chart {
+		
+		top: 900;
+		left: 900;
+		
+	}
+
+</style>
