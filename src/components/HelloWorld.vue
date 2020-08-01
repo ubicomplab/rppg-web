18edc5ec03337836a3a4ddfb5720efc849ac2d56
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-08-01 18:21:29 -0400

    add tf preprocess

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,43 @@
 <template>
-<div>	
-  <div id = "WebCam">
-	<video autodisplay="true" width="600" height="400" id="video"></video>
-	<!--canvas id = "canvas" height = "680" width = "100%"></canvas-->
-  </div>
-  
-  <div id = "visualization"></div>
-</div>
+	<div>
+		<div id="WebCam">
+			<video autodisplay="true" height="480" width="640" id="video"></video>
+			<!--canvas id = "TargetRegion" height = "480" width = "640"></canvas-->
+			<!--canvas id = "canvasFrame" height = "680" width = "100%"></canvas-->
+			
+		</div>
+
+		<div id="visualization"></div>
+	</div>
 </template>
 
 <script>
-import run from './sketch.js'	
+	import run from './sketch.js'
 
-export default {
-  name: 'WebCam',
-  props: {
-    msg: String
-  },
-  mounted() {
-	run();
-  }
-}
-
+	export default {
+		name: 'WebCam',
+		props: {
+			msg: String
+		},
+		mounted() {
+			
+			run();
+		},
+		methods: {
+			//getVideoDimensions(e) {
+				//console.log(e.target.videoHeight)
+				//console.log(e.target.videoWidth)
+			//}
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	
-
-		
 	#chart {
-		
+
 		top: 900;
 		left: 900;
-		
-	}
 
+	}
 </style>
