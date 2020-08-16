780ef4be29a790716117aa4bc14fc5c8e165fccf
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-08-16 18:30:57 -0400

    seperate files for custom layers

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,10 +1,16 @@
 import * as vis from 'vis-timeline/standalone';
 import * as tf from '@tensorflow/tfjs';
-//import * as tfn from '@tensorflow/tfjs-node';
+import {
+	AttentionMask
+} from './AttentionMask.js';
+import {
+	TSM
+} from './TSM.js';
 
 export default function run() {
 
 	let video = document.getElementById('video');
+	var path = "./model.json";
 	var orig_v, Xsub, dXsub, prevFrame;
 	var diffBatch, Batch;
 	var delay = 30;
@@ -14,39 +20,27 @@ export default function run() {
 	var prediction;
 	let model;
 
-	//const handler = tfn.io.fileSystem("./model.json");
-	var path = "./model.json";
-
-	//processModel();
+	tf.serialization.registerClass(TSM);
+	tf.serialization.registerClass(AttentionMask);
 
 	loadModel();
 	async function loadModel() {
-		//const handler = tfn.io.fileSystem("./model.json");
 		model = await tf.loadLayersModel(path);
-
+		/* eslint-disable no-console */
+		console.log("Successfully loaded ml model");
 	}
+	
 	async function processModel() {
-		//const handler = tfn.io.fileSystem("./model.json");
 		model = await tf.loadLayersModel(path);
-	//	model = await tf.loadLayersModel(path);
 		var result = model.predict(diffBatch, Batch);
 		prediction = result;
 	}
-	//processModel();
-
-	//const model = tf.loadLayersModel(path);
-
-
-	//const model = await tf.loadLayersModel(path);
-
-	//const model = tf.loadLayersModel('./model.json');
 
 	var names = ["uniform"];
 	var groups = new vis.DataSet();
 	var container = document.getElementById("visualization");
 	var dataset = new vis.DataSet();
 	var now = vis.moment();
-	//	var delay = 100;
 
 	var options = {
 		drawPoints: false,
@@ -85,30 +79,24 @@ export default function run() {
 
 	function loop() {
 		preprocess();
-		//	result =
-		//rppg(sample);
 		setTimeout(loop, delay);
 	}
 
 	function preprocess() {
 		orig_v = tf.browser.fromPixels(video);
-		orig_v.print(true);
 
 		Xsub = tf.image.resizeBilinear(orig_v, [dim, dim]);
-		Xsub.print(true);
 
 		Xsub = Xsub.asType('float32').div(tf.scalar(255));
 		Xsub = Xsub.expandDims(0); // (1, 36, 36, 3)
-		Xsub.print(true);
+
 
 
 		if (prevFrame == null) {
 			prevFrame = Xsub;
 			/* eslint-disable no-console */
 			console.log("initialize")
-			//batch_counter++;
-			//Batch = Xsub;
-			//cumSum = 0;
+			
 		} else {
 			//--------------------------------------
 			// didn't get  only the 300, 300 , ie didn't crop		
@@ -124,27 +112,14 @@ export default function run() {
 			console.log("below is X before mean")
 			console.log(Xsub.print(true));
 
-			// -------------------------------------
-			// get average frame here 
-			//Xsub = tf.mean(Xsub, 0); // (1, 36, 36, 3)
 			prevFrame = Xsub;
 			if (batch_counter == 0) {
 				Batch = tf.cast(Xsub, 'float32');
 				diffBatch = dXsub;
-				/* eslint-disable no-console */
-				console.log("below is batch");
-				console.log(Batch.print(true));
 			} else {
-
-				//Batch = tf.expandDims(Batch, 0);
-				//Xsub = tf.expandDims(Xsub, 0);
-				/* eslint-disable no-console */
-				console.log("X in stack")
-				console.log(Xsub.print(true));
-				/* eslint-disable no-console */
-				console.log("batch in stack")
-				console.log(Batch.print(true));
-
+				Batch.print(true);
+				diffBatch.print(true);
+				
 				Batch = tf.concat([Batch, Xsub]) // note the xsub here is after 
 				diffBatch = tf.concat([diffBatch, dXsub]);
 
@@ -155,7 +130,6 @@ export default function run() {
 		if (batch_counter == batch_size) {
 
 			// call update the chart
-
 			Batch = tf.transpose(Batch, [2, 1, 3, 0]); // swap axis
 			Batch = tf.expandDims(Batch, 0); // expand dimension
 
@@ -194,8 +168,7 @@ export default function run() {
 	// the chart
 	function addDataPoint() {
 		var now = vis.moment();
-		//var yVal = 1 + Math.round(Math.random() * 10);
-
+		
 		console.log("inside prediction");
 		console.log(prediction);
 		dataset.add([{
@@ -231,117 +204,3 @@ export default function run() {
 		}
 	}
 }
-/*var names, groups, container, dataset, now, options, graph2d;
-
-function initialize_chart() {
-	var names = ["uniform"];
-	var groups = new vis.DataSet();
-	var container = document.getElementById("visualization");
-	var dataset = new vis.DataSet();
-	var now = vis.moment();
-	//	var delay = 100;
-
-	var options = {
-		drawPoints: false,
-		dataAxis: {
-			visible: true,
-			left: {
-				title: {
-					text: "Normalized Amplitude",
-				}
-			}
-		},
-		legend: false,
-		start: vis.moment().add(-5, "seconds"), // display start,  end
-		end: vis.moment().add(10, "seconds"),
-
-	};
-
-	// eslint-disable-next-line
-	var graph2d = new vis.Graph2d(container, dataset, groups, options);
-
-	// could delete the group , just use dataset
-	groups.add({
-		id: 0,
-		content: names[0],
-		options: {
-			drawPoints: false,
-			interpolation: {
-				parametrization: "uniform",
-			},
-		},
-	});
-}*/
-//import * as cv from 'opencv'
-/*const cv = require('opencv4nodejs');
-
-
-
-cv['onRuntimeInitialized']=()=>{
-          let mat = new cv.Mat();
-          console.log(mat.size());
-          mat.delete();
-};
-	cv['onRuntimeInitialized'] = () => {
-		var startTime = Date.now();
-		//var dim = 36;
-		let src = new cv.Mat(video.height, video.width, cv.CV_32FC3);
-		let dst = new cv.Mat(video.height, video.width, cv.CV_32FC3);
-		let cap = new cv.VideoCapture(video);
-		//let zeroMat = new cv.Mat.zeros(dim, dim, cv.CV_32FC3);
-
-		cap.read(src); // src stores the current frame , shape(480, 640, 3(type->float))
-
-		let rect = new cv.Rect(160, 90, 300, 300);
-		dst = src.roi(rect); // cropped data here
-		console.log(dst.rows);
-		
-		while success:
-		      t.append(vidObj.get(cv2.CAP_PROP_POS_MSEC))# current timestamp in milisecond 
-		      vidLxL = cv2.resize(img_as_float(img[:, int(width/2) - int(height/2 + 1) : int(height/2) + int(width/2), :]), (L, L))
-		      plt.imshow(cv2.rotate(vidLxL, cv2.ROTATE_90_CLOCKWISE))
-		      vidLxL[vidLxL > 1] = 1
-		      vidLxL[vidLxL < (1/255)] = 1/255
-		      Xsub[i, :, :, :] = vidLxL
-		      success, img = vidObj.read() # read the next one
-		      i = i + 1
-		
-
-
-		const FPS = 30;
-
-		preprocess();
-		console.log("happy")
-		let endTime = Date.now();
-		console(endTime - startTime);
-
-		function preprocess() {
-			try {
-				//if (!streaming) {
-				// clean and stop.
-				//		src.delete();
-				//		dst.delete();
-				//		return;
-				//	}
-
-				let begin = Date.now();
-				// start processing.
-				//	let vidLxL = cv2.resize(src, dst, )
-
-
-				cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-				cv.imshow('canvasOutput', dst);
-				// schedule the next one.
-				let delay = 1000 / FPS - (Date.now() - begin);
-				setTimeout(preprocess, delay);
-			} catch (err) {
-				//utils.printError(err);
-			}
-		}
-		src.delete();
-		dst.delete();
-		cap.delete();
-		rect.delete();
-
-	};
-*/
