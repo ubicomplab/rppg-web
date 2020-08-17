95953a0aceb23871791d72b19959e64204fedc40
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-08-17 18:12:21 -0400

    update TSM and AttentionMask

--- src/components/TSM.js
+++ src/components/TSM.js
@@ -2,57 +2,73 @@ import * as tf from '@tensorflow/tfjs';
 
 
 export class TSM extends tf.layers.Layer {
-	static className = 'TSM';
 
 	constructor(config) {
 		super(config);
 	}
 
 	computeOutputShape(inputShape) {
+		/* eslint-disable no-console */
+	//	console.log("TSM expected outputshape");
+	//	console.log(this.name);
+		console.log(inputShape);
+
 		return inputShape;
 	}
 
 	// eslint-disable-next-line
-	call(input, n_frame, nb_filters, kernel_size, padding, activation) {
-		return tf.tidy()(() => {
-			/* eslint-disable no-console */
-			console.log("TSM is started execution");
-			/* eslint-disable no-console */
-			console.log(input.shape);
-			let nt, h, w, c;
-			let out, out1, out2, out3, empty;
-			let padding1, padding2;
-			const fold_div = 3;
-			const fold = Math.floor(c / fold_div);
-			const last_fold = c - (fold_div - 1) * fold;
-			nt, h, w, c = input.shape;
-			input = tf.reshape(input, [n_frame, h, w, c]);
-			input = tf.expandDims(input, 0);
-			out1, out2, out3 = tf.split(input, [fold, fold, last_fold], -1)
+	call(inputs) {
 
-			// Shift left
-			padding1 = tf.zerosLike([-1, 1, h, w, fold])
-			padding1 = tf.expandDims(padding1, 1)
-			empty, out1 = tf.split(out1, [1, n_frame - 1], 1)
-			out1 = tf.concat([out1, padding1], 1)
+		var input = inputs[0];
+		const n_frame = 20;
+		input.print(1);
+		/* eslint-disable no-console */
+		console.log(inputs);
+		
+		console.log("TSM is started execution");
+		/* eslint-disable no-console */
+		console.log(input.shape);
+		let nt, h, w, c;
+		let out, out1, out2, out3, empty;
+		let padding1, padding2;
+		nt = input.shape[0];
+		h = input.shape[1];
+		w = input.shape[2];
+		c = input.shape[3];
+		
+		const fold_div = 3;
+		const fold = Math.floor(c / fold_div);
+		const last_fold = c - (fold_div - 1) * fold;
+		console.log(fold)
+		input = tf.reshape(input, [nt, h, w, c]);
+		input = tf.expandDims(input, 0);
+		[out1, out2, out3] = tf.split(input, [fold, fold, last_fold], -1)
 
-			// shift right
-			padding2 = tf.zerosLike([-1, 1, h, w, fold])
-			padding2 = tf.expandDims(padding2, 1)
-			out2, empty = tf.split(out2, [n_frame - 1, 1], 1)
-			out2 = tf.concat([padding2, out2], 1)
+		// Shift left
+		padding1 = tf.zeros([out1.shape[0], 1, out1.shape[2], out1.shape[3], fold]);
+		
+		console.log(padding1);
+		[empty, out1] = tf.split(out1, [1, n_frame - 1], 1);
+		console.log(out1);
+		console.log(empty);
+		out1 = tf.concat([out1, padding1], 1);
+		console.log(out1);
 
-			out = tf.concat([out1, out2, out3], -1)
-			out = tf.reshape(out, (-1, h, w, c))
-			out = tf.layers.conv2D({
-				filters: nb_filters,
-				kernelSize: kernel_size,
-				padding: padding,
-				activation: activation
-			}).apply(out)
+		// shift right
+		padding2 = tf.zeros([out2.shape[0], 1, out2.shape[2], out2.shape[3], fold]);
+		[out2, empty] = tf.split(out2, [n_frame - 1, 1], 1);
+		out2 = tf.concat([padding2, out2], 1);
+		console.log(out2);
 
-			return out;
-		});
+		out = tf.concat([out1, out2, out3], -1);
+		console.log(out);
+		out = tf.reshape(out, [-1, h, w, c]);
+
+		console.log("TSM is called, following is its output");
+		out.print(1);
+
+		return out;
+
 	}
 
 	getConfig() {
