03247d5d20d4e054f70f2e36e71372b3dc48ff28
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-08-18 17:07:44 -0400

    add custom layer python code

---
+++ python/custom_layers/AttentionMask.py
@@ -0,0 +1,11 @@
+class Attention_mask(tf.keras.layers.Layer):
+    def call(self, x):
+        xsum = K.sum(x, axis=1, keepdims=True)
+        xsum = K.sum(xsum, axis=2, keepdims=True)
+        xshape = K.int_shape(x)
+        return x / xsum * xshape[1] * xshape[2] * 0.5
+
+    def get_config(self):
+        config = super(Attention_mask, self).get_config()
+        return config
+
---
+++ python/custom_layers/TSM.py
@@ -0,0 +1,30 @@
+class TSM(tf.keras.layers.Layer):
+    def call(self, x, n_frame, fold_div=3):
+        nt, h, w, c = x.shape
+        x = K.reshape(x, (-1, n_frame, h, w, c))
+        fold = c // fold_div
+        last_fold = c - (fold_div-1)*fold
+        out1, out2, out3 = tf.split(x, [fold, fold, last_fold], axis=-1)
+
+        # Shift left
+        padding_1 = tf.zeros_like(out1)
+        padding_1 = padding_1[:,-1,:,:,:]
+        padding_1 = tf.expand_dims(padding_1, 1)
+        _, out1 = tf.split(out1, [1, n_frame-1], axis=1)
+        out1 = tf.concat([out1, padding_1], axis=1)
+
+        # Shift right
+        padding_2 = tf.zeros_like(out2)
+        padding_2 = padding_2[:,0,:,:, :]
+        padding_2 = tf.expand_dims(padding_2, 1)
+        out2, _ = tf.split(out2, [n_frame-1,1], axis=1)
+        out2 = tf.concat([padding_2, out2], axis=1)
+
+        out = tf.concat([out1, out2, out3], axis=-1)
+        out = K.reshape(out, (-1, h, w, c))
+
+        return out
+
+    def get_config(self):
+        config = super(TSM, self).get_config()
+        return config
