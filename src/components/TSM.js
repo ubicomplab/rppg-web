c579f8c449fc3fa5cb5692113200ddef6415fabb
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-08-18 13:54:47 -0400

    frame update sync

--- src/components/TSM.js
+++ src/components/TSM.js
@@ -8,7 +8,7 @@ export class TSM extends tf.layers.Layer {
 
 	computeOutputShape(inputShape) {
 		/* eslint-disable no-console */
-		console.log(inputShape);
+		//console.log(inputShape);
 		return inputShape;
 	}
 
@@ -17,14 +17,15 @@ export class TSM extends tf.layers.Layer {
 
 		var input = inputs[0];
 		const n_frame = 20;
-		input.print(1);
+		//input.print(1);
 		/* eslint-disable no-console */
 		//console.log(inputs);
 		
-		console.log("TSM is started execution");
+		//console.log("TSM is started execution");
 		/* eslint-disable no-console */
-		console.log(input.shape);
+		//console.log(input.shape);
 		let nt, h, w, c;
+		// eslint-disable-next-line
 		let out, out1, out2, out3, empty;
 		let padding1, padding2;
 		nt = input.shape[0];
@@ -35,7 +36,7 @@ export class TSM extends tf.layers.Layer {
 		const fold_div = 3;
 		const fold = Math.floor(c / fold_div);
 		const last_fold = c - (fold_div - 1) * fold;
-		console.log(fold)
+		//console.log(fold)
 		input = tf.reshape(input, [nt, h, w, c]);
 		input = tf.expandDims(input, 0);
 		[out1, out2, out3] = tf.split(input, [fold, fold, last_fold], -1)
@@ -43,12 +44,12 @@ export class TSM extends tf.layers.Layer {
 		// Shift left
 		padding1 = tf.zeros([out1.shape[0], 1, out1.shape[2], out1.shape[3], fold]);
 		
-		console.log(padding1);
+		//console.log(padding1);
 		[empty, out1] = tf.split(out1, [1, n_frame - 1], 1);
-		console.log(out1);
-		console.log(empty);
+		//console.log(out1);
+		//console.log(empty);
 		out1 = tf.concat([out1, padding1], 1);
-		console.log(out1);
+		//console.log(out1);
 
 		// Shift right
 		padding2 = tf.zeros([out2.shape[0], 1, out2.shape[2], out2.shape[3], fold]);
