a3b42fcc8baa01fe2a13534a40d2a37129905039
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-08-28 05:03:29 -0400

    update custom layers

--- src/components/AttentionMask.js
+++ src/components/AttentionMask.js
@@ -1,27 +1,26 @@
 import * as tf from '@tensorflow/tfjs';
 
 export class AttentionMask extends tf.layers.Layer {
+	
 	static className = 'AttentionMask';
+	
 	constructor(config) {
 		super(config);
 	}
 
 	computeOutputShape(inputShape) {
-		/* eslint-disable no-console */
-		this.inputShape = inputShape;
 		return [inputShape[0], inputShape[1], inputShape[2], inputShape[3]];
 	}
 
-	// eslint-disable-next-line
-	call(inputs, kwargs) {
-		/* eslint-disable no-console */
-		var input = inputs[0];
-		var inputSum = tf.sum(input, 1, true);
+	call(inputs) {
+		return tf.tidy(() => {
+			var input = inputs[0];
+			var inputSum = tf.sum(input, 1, true);
+			inputSum = tf.sum(inputSum, 2, true);
+			var out = input.div(inputSum).mul(input.shape[1]).mul(input.shape[2]).mul(0.5);
 
-		inputSum = tf.sum(inputSum, 2, true);
-		var out = input.div(inputSum).mul(input.shape[1]).mul(input.shape[2]).mul(0.5);
-			
-		return out;
+			return out;
+		});
 	}
 
 	getConfig() {
@@ -34,4 +33,3 @@ export class AttentionMask extends tf.layers.Layer {
 	}
 }
 
-AttentionMask.className = 'AttentionMask'; // static variable
