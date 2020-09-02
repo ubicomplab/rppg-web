f546cf9cece9418c36e4e422eb28a339bc177ecb
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-09-01 23:40:22 -0400

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-	<p>RPPG (remote photoplethysmography)</p>
-    <WebCam msg="RPPG (remote photoplethysmography)"/>
-  </div>
+	<div id="app">
+		<WebCam msg="RPPG" />
+	</div>
 </template>
 
 <script>
-import WebCam from './components/LandingPage.vue'
-
-export default {
-  name: 'App',
-  components: {
-    WebCam
-  }
-}
+	import WebCam from './components/LandingPage.vue'
+	
+	export default {
+		name: 'App',
+		components: {
+			WebCam
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		margin-top: -0px !important; 
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
