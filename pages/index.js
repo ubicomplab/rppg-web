d6380e7ae2109840287c6e58b988ab7714481a1b
Jucong <hejucong1995@gmail.com>
2020-09-08 22:42:59 -0400

    add video

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import Webcam from 'react-webcam';
+import { browser } from '@tensorflow/tfjs';
 import Header from '../components/header';
 import Research from '../components/research';
+import styles from '../styles/Home.module.scss';
 
-export default function Home() {
+const Home = () => {
+  const webcamRef = React.useRef(null);
+  const [interValeId, setIntervalId] = useState(null);
+  const [isRecording, setRecording] = useState(false);
+
+  useEffect(
+    () => () => {
+      clearInterval(interValeId);
+    },
+    [interValeId]
+  );
+
+  const handleRecording = () => {
+    if (!isRecording) {
+      const id = setInterval(capture, 20);
+      setIntervalId(id);
+    } else {
+      clearInterval(interValeId);
+    }
+    setRecording(!isRecording);
+  };
+
+  const capture = () => {
+    if (webcamRef) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      const img = new Image(36, 36);
+      img.src = imageSrc;
+      const origV = browser.fromPixels(img);
+      console.log(origV);
+    }
+  };
+
   return (
-    <div>
+    <>
       <Head>
         <title>RPPG</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Research />
-    </div>
+      <div className={styles.contentContainer}>
+        <Research />
+        {isRecording && (
+          <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+        )}
+        <button
+          className={styles.recordingButton}
+          onClick={handleRecording}
+          type="button"
+        >
+          {isRecording ? 'Stop Recording' : 'Start Recording'}
+        </button>
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
