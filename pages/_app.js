a5ca950bb41ea8056a056efc6d6f73c94a57557c
Jucong <hejucong1995@gmail.com>
2020-09-08 21:49:41 -0400

    add basic layout

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
 }
 
 MyApp.propTypes = {
-  Component: PropTypes.node,
+  Component: PropTypes.any,
   pageProps: PropTypes.object
 };
 
--- pages/index.js
+++ pages/index.js
@@ -4,63 +4,49 @@ import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>RPPG</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      <header className={styles.header}>
+        <a href="http://www.washington.edu/">
+          <img
+            src="/images/UWlogo3.png"
+            alt="UW Logo"
+            width="90"
+            height="60"
+            className="UW-logo"
+          />
+          <img
+            src="/images/UWlogo2.png"
+            alt="UW Logo"
+            width="260"
+            height="24"
+            className="UW-text"
+          />
         </a>
-      </footer>
+      </header>
+      <div>
+        <h3>
+          <b>Research Paper</b>
+        </h3>
+        <b>
+          <p>
+            Multi-Task Temporal Shift Attention Networks for On-Device
+            Contactless Vitals Measurement
+          </p>
+        </b>
+        <a href="https://homes.cs.washington.edu/~xliu0/">Xin Liu</a>,
+        <a href="https://www.jwfromm.com/">Josh Fromm</a>,
+        <a href="https://homes.cs.washington.edu/~shwetak/">Shwetak Patel</a>,
+        <a href="https://www.microsoft.com/en-us/research/people/damcduff/">
+          Daniel McDuff
+        </a>
+        <a href="NIPS2020_MTTS_CAN_preprint.pdf">
+          <h3>Download PDF</h3>
+        </a>
+      </div>
     </div>
   );
 }
