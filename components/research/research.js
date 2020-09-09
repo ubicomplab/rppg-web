2ebb3f1a71f84814a8f61f8c0892c2fbc1d4bf0f
Jucong <hejucong1995@gmail.com>
2020-09-08 21:54:38 -0400

    breke up components

---
+++ components/research/research.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Research = () => (
+  <div>
+    <h3>
+      <b>Research Paper</b>
+    </h3>
+    <b>
+      <p>
+        Multi-Task Temporal Shift Attention Networks for On-Device Contactless
+        Vitals Measurement
+      </p>
+    </b>
+    <a href="https://homes.cs.washington.edu/~xliu0/">Xin Liu</a>,
+    <a href="https://www.jwfromm.com/">Josh Fromm</a>,
+    <a href="https://homes.cs.washington.edu/~shwetak/">Shwetak Patel</a>,
+    <a href="https://www.microsoft.com/en-us/research/people/damcduff/">
+      Daniel McDuff
+    </a>
+    <a href="NIPS2020_MTTS_CAN_preprint.pdf">
+      <h3>Download PDF</h3>
+    </a>
+  </div>
+);
+
+export default Research;
