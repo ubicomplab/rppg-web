9895d8ebd14e00940252854c315c6c0f5580268b
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-09-15 13:26:20 -0400

    add postprocess

--- ultils/tensorStore.js
+++ ultils/tensorStore.js
@@ -1,6 +1,10 @@
 class TensorStore {
   constructor() {
+    this.sum = 0;
+    this.MovingAvg = 0;
+    this.dataSet = [];
     this.tensors = [];
+    this.batchSize = 20;
   }
 
   getTensor() {
@@ -13,6 +17,30 @@ class TensorStore {
 
   addTensor(tensor) {
     this.tensors.push(tensor);
+  }
+
+  getSum() {
+    return this.sum;
+  }
+
+  getData() {
+    if (this.dataSet.length) {
+      return this.dataSet.shift();
+    }
+    return null;
+  }
+
+  getMovingAvg() {
+    return this.MovingAvg;
+  }
+
+  addData(data) {
+    if (this.dataSet.length === this.batchSize) {
+      this.sum -= this.getData();
+    }
+    this.sum += data;
+    this.dataSet.push(data);
+    this.MovingAvg = this.sum / this.dataSet.length;
   }
 }
 
