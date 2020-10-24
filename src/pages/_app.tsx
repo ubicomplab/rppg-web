c5e80dc6137032aab5e39d45cbead712659e546d
Jucong <hejucong1995@gmail.com>
2020-10-23 22:37:54 -0400

    migrate to ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.any,
-  pageProps: PropTypes.object
-};
 
 export default MyApp;
